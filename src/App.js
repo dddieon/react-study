adfcaa133293e35a12c875c2f91c922fc0ef3318
DDDieon <59174247+DDDieon@users.noreply.github.com>
2020-10-11 07:34:22 +0900

    201011 input상태관리

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { useState } from "react"
 
 function App() {
-    const [result, setResult] = useState(0)
-    const up = () => {
-        setResult(result + 1)
+    const [firstValue, setFirstValue] = useState("")
+    const [secondValue, setSecondValue] = useState("")
+    const [result, setResult] = useState("")
+    const calculate = (event) => {
+        event.preventDefault()
+        const {
+            target: { name },
+        } = event
+        console.log(name)
+        if (name === "plus") {
+            setResult(firstValue + secondValue)
+            alert(firstValue + secondValue)
+        }
+        if (name === "minus") {
+            setResult(firstValue - secondValue)
+            alert(firstValue - secondValue)
+        }
     }
-    const down = () => {
-        setResult(result - 1)
+    const onChange = (event) => {
+        const {
+            target: { value, name },
+        } = event
+        if (name === "firstText") {
+            setFirstValue(Number(value))
+        } else if (name === "secondText") {
+            setSecondValue(Number(value))
+        }
+    }
+    const submited = (event) => {
+        event.preventDefault()
+        alert(result)
     }
     return (
         <div className="App">
-            <h1>결과값 = {result}</h1>
-            <input type="button" onClick={up} value="더하기 +"></input>
-            <input type="button" onClick={down} value="빼기 -"></input>
+            <form onSubmit={submited}>
+                <input
+                    type="text"
+                    value={firstValue}
+                    name={"firstText"}
+                    onChange={onChange}
+                    required
+                ></input>
+                <input
+                    type="text"
+                    value={secondValue}
+                    name={"secondText"}
+                    onChange={onChange}
+                    required
+                ></input>
+                <hr></hr>
+                <button name="plus" onClick={calculate}>
+                    +
+                </button>
+                <button name="minus" onClick={calculate}>
+                    -
+                </button>
+            </form>
+            <p>첫번째 값 : {firstValue}</p>
+            <p>두번째 값 : {secondValue}</p>
+            <h1>현재 결과 : {result}</h1>
         </div>
     )
 }
