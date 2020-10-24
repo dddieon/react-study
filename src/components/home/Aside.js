20f65d36453ec819aa48e0273a239fcc43facae7
Jiwon <ajiop0123@gmail.com>
2020-10-24 21:13:48 +0900

    Board 컴포넌트 CSS 작업 (+반응형)

--- src/components/home/Aside.js
+++ src/components/home/Aside.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const AsideTag = styled.aside`
   font-weight: bold;
@@ -11,7 +12,9 @@ const AsideTag = styled.aside`
 const Aside = () => {
   return (
     <AsideTag>
-      에어비앤비의 코로나19 대응 방안에 대한 최신 정보를 확인하세요.
+      <Link>
+        에어비앤비의 코로나19 대응 방안에 대한 최신 정보를 확인하세요.
+      </Link>
     </AsideTag>
   );
 };
