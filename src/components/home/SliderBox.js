ae09ea92a1d92e8445cee981d61e9f5025dcf05e
DDDieon <59174247+DDDieon@users.noreply.github.com>
2020-10-25 22:30:49 +0900

    Add Sub-Components Of MainSlider

---
+++ src/components/home/SliderBox.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SliderBox = ({ src, image, className, title, desc }) => {
+  const Box = styled.a`
+    display: block;
+    width: 272px;
+    @media (min-width: 950px) {
+      width: 30%;
+    }
+    margin: 8px;
+    border-radius: 16px;
+    overflow: hidden;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
+    img {
+      display: block;
+      width: 100%;
+    }
+    .images__main--desc {
+      padding: 12px 16px 16px;
+      div {
+        font-size: 18px;
+        font-weight: 600;
+      }
+      p {
+        font-size: 14px;
+      }
+    }
+  `;
+  return (
+    <>
+      <Box to={src}>
+        <img src={image} alt="설명"></img>
+        <div className={className}>
+          <div>{title}</div>
+          <p>{desc}</p>
+        </div>
+      </Box>
+    </>
+  );
+};
+
+export default SliderBox;
